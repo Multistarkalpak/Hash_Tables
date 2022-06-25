35ffdb0229c405f46f84f914b04d6e9232c9e914
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-25 14:05:48 +0530

    Remove the word from paragarph

--- HashTables/HashTables/Program.cs
+++ HashTables/HashTables/Program.cs
@@ -23,7 +23,29 @@
             }
             int frequency = LinkedHashMap.Get("to");
             Console.WriteLine(frequency);
+            //<UC2>
+            string Paragraph = "“Paranoids are not paranoid because they are " +
+                "paranoid but because they keep putting themselves deliberately into" +
+                " paranoid avoidable situations";
+            string[] letters = Paragraph.ToLower().Split(" ");
 
+            foreach (string word in letters)
+            {
+                int value = LinkedHashMap.Get(word);
+                if (value == default)
+                {
+                    value = 1;
+                }
+                else value += 1;
+                LinkedHashMap.Add(word, value);
+            }
+            int frequency1 = LinkedHashMap.Get("paranoid");
+            Console.WriteLine(frequency1);
+            //uc3
+            LinkedHashMap.Remove("avoidable");
+            int frequency2 = LinkedHashMap.Get("avoidable");
+
+            Console.WriteLine(frequency2);
 
 
         }
